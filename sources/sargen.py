6ceceb5318ebae4ac7f0276a826818f527d2e06b
baterflyrity <baterflyrity@yandex.ru>
2020-10-01 15:02:01 +0300

    Removed nptyping requirement and moved type hints of arrays to parameters' documentation.
    Signed-off-by: baterflyrity <baterflyrity@yandex.ru>

--- sources/sargen.py
+++ sources/sargen.py
@@ -4,77 +4,53 @@ Optic to SAR image converter.
 Generates radio location 24 bit grayscale (LLL) images from 24 bit RGB optic images with random transformations (noise, rotation, scale,tilt).
 """
 import math
+from numbers import Number
 from pathlib import Path
-from typing import Any, Union
+from typing import Union
 
-import numpy as np
-from nptyping import NDArray, Number
+import numpy as np  # TODO: update to 1.20, see https://github.com/numpy/numpy/milestone/83
 from PIL import Image
 
 
-def _assert_array(array: np.ndarray, signature: NDArray) -> None:
-	"""
-	Assert given object is array of defined shape and data type.
-
-	Raises ValueError.
-
-	:param array: object to check.
-	:param signature: required shape and data type of array.
-	"""
-	try:
-		if not isinstance(array, np.ndarray):
-			raise
-		# noinspection PyTypeChecker
-		if not isinstance(array, signature):
-			# noinspection PyProtectedMember,PyTypeChecker
-			if not isinstance(array.astype(signature._type.npbase or signature._type.base, casting='safe'), signature):
-				raise
-	except Exception as exception:
-		raise ValueError(f'Array must be {signature}.') from exception
-
-
-def clamp_array(array: NDArray[Number], minimum: Number = 0, maximum: Number = 255) -> NDArray[Number]:
+def clamp_array(array: np.ndarray, minimum: Number = 0, maximum: Number = 255) -> np.ndarray:
 	"""
 	Clamp values less then 0 to 0 and greater then 255 to 255.
 
 	:param array: array of numbers.
 	:param minimum: minimum value to clamp elements below it.
 	:param maximum: maximum value to clamp elements above it.
+	:return: arrays of same shape with clamped values.
 	"""
-	# noinspection PyTypeChecker
-	# _assert_array(array, NDArray[Number])
 	# noinspection PyTypeChecker
 	return np.where(array < minimum, minimum, np.where(array > maximum, maximum, array))
 
 
-def convert_image(image: NDArray[(Any, Any, 3), float], *, channels_weights: NDArray[3, float] = np.array([0.299, 0.557, 0.144]), bias: float = -33) -> NDArray[(Any, Any), float]:
+def convert_image(image: np.ndarray, *, channels_weights: np.ndarray = np.array([0.299, 0.557, 0.144]), bias: float = -33) -> np.ndarray:
 	"""
 	Convert R, G, B color channels to luma single channel.
 
 	Note that luma is not clamped.
 
-	:param image: array of pixels of WxHxCh shape.
-	:param channels_weights: R, G, B coefficients.
+	:param image: array of pixels of shape (height, width, 3) of floats.
+	:param channels_weights: R, G, B coefficients array of shape 3 of floats.
 	:param bias: luma bias to apply after conversion.
+	:return: array of shape (height, width) of floats.
 	"""
-	# noinspection PyTypeChecker
-	# _assert_array(image, NDArray[(Any, Any, 3), float])
 	return (image * channels_weights).sum(axis=2) + bias
 
 
-def noise_image(image: NDArray[(Any, Any), float], *, mean: float = 0, deviation: float = 12.5, generator: np.random.Generator = np.random.default_rng()) -> NDArray[(Any, Any), float]:
+def noise_image(image: np.ndarray, *, mean: float = 0, deviation: float = 12.5, generator: np.random.Generator = np.random.default_rng()) -> np.ndarray:
 	"""
 	Add gaussian noise to each pixel of single channel image.
 
 	Note that luma is not clamped.
 
-	:param image: array of pixels of WxHxCh shape.
+	:param image: array of pixels of shape (height, width) of floats.
 	:param mean: mu parameter of normal distribution.
 	:param deviation: sigma parameter of normal distribution also known as standard deviation.
 	:param generator: random number generator.
+	:return: array of shape (height, width) of floats.
 	"""
-	# noinspection PyTypeChecker
-	# _assert_array(image, NDArray[(Any, Any), float])
 	return image + generator.normal(mean, deviation, image.shape)
 
 
@@ -112,19 +88,16 @@ def scale_image(image: Image.Image, *, mean: float = 1, deviation: float = 0.2, 
 	return image.resize(tuple((np.array(image.size) * factor).round(0).astype(int)))
 
 
-def _find_perspective_coefficients(origin_points: NDArray[(4, 2), float], transformed_points: NDArray[(4, 2), float]) -> NDArray[8, float]:
+def _find_perspective_coefficients(origin_points: np.ndarray, transformed_points: np.ndarray) -> np.ndarray:
 	"""
 	Find perspective transformation coefficients a, b, c, d, e, f, g, h.
 
 	See https://stackoverflow.com/a/14178717 along with other answers and comments.
 
-	:param origin_points: 4 origin points that are mapped to transformed.
-	:param transformed_points: 4 points to where origin ones are mapped.
+	:param origin_points: 4 origin points that are mapped to transformed (as array of shape (4, 2) of floats).
+	:param transformed_points: 4 points to where origin ones are mapped (as array of shape (4, 2) of floats).
+	:return: array of shape 8 of floats.
 	"""
-	# noinspection PyTypeChecker
-	# _assert_array(origin_points, NDArray[(4, 2), float])
-	# noinspection PyTypeChecker
-	# _assert_array(transformed_points, NDArray[(4, 2), float])
 	matrix_buf = []
 	for src_point, dist_point in zip(origin_points, transformed_points):
 		matrix_buf.append([src_point[0], src_point[1], 1, 0, 0, 0, -dist_point[0] * src_point[0], -dist_point[0] * src_point[1]])
@@ -149,12 +122,11 @@ def tilt_image(image: Image.Image, *, deviation: float = 0.2, generator: np.rand
 	"""
 	# See https://stackoverflow.com/a/14178717 along with other answers and comments.
 	# We have 4 points = 8 coordinates that can be shifted (each in one direction only).
-	# noinspection PyTypeChecker
 	factors = -1 * clamp_array(generator.uniform(0, deviation * min(image.size), 8), minimum=0, maximum=deviation * min(image.size))  # invert factors to stretch image out of the clipping box
 	# Shift image box points out of self by factors to avoid empty space after perspective.
 	origin_points = np.array([[0, 0], [image.width, 0], [image.width, image.height], [0, image.height]])
 	transformed_points = np.array([[-factors[0], -factors[1]], [image.width + factors[2], -factors[3]], [image.width + factors[4], image.height + factors[5]], [-factors[6], image.height + factors[7]]])
-	# noinspection PyTypeChecker
+
 	transformation_parameters = _find_perspective_coefficients(origin_points, transformed_points)
 	return image.transform(image.size, Image.PERSPECTIVE, tuple(transformation_parameters), Image.BICUBIC)
 
@@ -185,7 +157,7 @@ def transform_image(image: Image.Image, *, rotate: bool = True, scale: bool = Tr
 	return image
 
 
-def sar(image: Union[str, Path, Image.Image, NDArray[(Any, Any, 3), Number]], *, noise: bool = True, transform: bool = True, channels_weights: NDArray[3, float] = np.array([0.299, 0.557, 0.144]), bias: float = -33, noise_mean: float = 0, noise_deviation: float = 12.5, scale_mean: float = 1, scale_deviation: float = 0.2, tilt_deviation: float = 0.2, generator: np.random.Generator = np.random.default_rng()) -> Image.Image:
+def sar(image: Union[str, Path, Image.Image, np.ndarray], *, noise: bool = True, transform: bool = True, channels_weights: np.ndarray = np.array([0.299, 0.557, 0.144]), bias: float = -33, noise_mean: float = 0, noise_deviation: float = 12.5, scale_mean: float = 1, scale_deviation: float = 0.2, tilt_deviation: float = 0.2, generator: np.random.Generator = np.random.default_rng()) -> Image.Image:
 	"""
 	Generate SAR image from optic one.
 
@@ -193,10 +165,10 @@ def sar(image: Union[str, Path, Image.Image, NDArray[(Any, Any, 3), Number]], *,
 
 	Generation parameters can be tuned with keyword arguments.
 
-	:param image: text of path to image file or path object or image object or array.
+	:param image: text of path to image file or path object or image object or array of shape (height, width, 3) of numbers.
 	:param noise:  whether to apply gaussian noise.
 	:param transform: whether to apply random transformations.
-	:param channels_weights: R, G, B coefficients.
+	:param channels_weights: R, G, B coefficients array of shape 3 of floats.
 	:param bias: luma bias to apply after conversion.
 	:param noise_mean: mu parameter of normal distribution in case noise is True.
 	:param noise_deviation: sigma parameter of normal distribution also known as standard deviation in case noise is True.
@@ -221,7 +193,6 @@ def sar(image: Union[str, Path, Image.Image, NDArray[(Any, Any, 3), Number]], *,
 			raise ValueError(f'Image must has shape (..., ..., 3) but has {shape}.')
 		image = image[:, :, :3]
 	image = image.astype(float)
-	# noinspection PyTypeChecker
 	image = convert_image(image, channels_weights=channels_weights, bias=bias)
 	if noise:
 		image = noise_image(image, mean=noise_mean, deviation=noise_deviation, generator=generator)
--- tests/test_sargen.py
+++ tests/test_sargen.py
@@ -1,34 +1,13 @@
 """
 Unit tests for SAR generator functions.
 """
-from typing import Any, Tuple
+import math
+from typing import Tuple
 
-# noinspection PyProtectedMember
-from sargen.sources.sargen import _assert_array
-from sargen.sources.sargen import *
-from nptyping import NDArray
 import numpy as np
+from PIL import Image
 
-
-def test__assert_array():
-	"""
-	Validate different types, shapes and arrays' data types.
-	"""
-	valid_objects = np.array([[1, 2]]), np.array([[1.0, 2.0], [1.2, 2.5]], dtype=np.int32)
-	invalid_objects = None, 1, np.array([1, 2]), np.array([[1, 2, 3]]), np.array([[1.0, 2.0], [1.2, 2.5]]), np.array([[1.0, 2.0], [1.2, 2.5]], dtype=np.int64), np.array([[1.0, 2.0], [1.2, 2.5]], dtype=np.int16)
-	signature = NDArray[(Any, 2), np.int32]
-	for obj in valid_objects:
-		# noinspection PyTypeChecker
-		assert _assert_array(obj, signature) is None, f'Object {obj} has shape {signature} and must be valid.'
-	for obj in invalid_objects:
-		exception = None
-		try:
-			# noinspection PyTypeChecker
-			_assert_array(obj, signature)
-		except ValueError as error:
-			exception = error
-		finally:
-			assert exception is not None, f'Object {obj} does not have shape {signature} and must be invalid.'
+from sargen.sources.sargen import clamp_array, convert_image, noise_image, rotate_image, sar, scale_image, tilt_image, transform_image
 
 
 def test_clamp_array():
@@ -62,9 +41,11 @@ def test_noise_image():
 	assert round(data.mean()) == 100 and round(data.std()) == 20
 
 
-def _find_white_dot(array: NDArray[np.uint8]) -> Tuple[Tuple[int, int], float]:
+def _find_white_dot(array: np.ndarray) -> Tuple[Tuple[int, int], float]:
 	"""
 	Find white dot in array (0 is black, >0 is white) and return its coordinates and angle in radians (0 - 2Pi).
+
+	:param array: array of unsigned bytes.
 	"""
 	y = array.argmax(axis=0).max()
 	x = array.argmax(axis=1).max()
@@ -89,7 +70,7 @@ def test_rotate_image():
 		size = np.array(rotated_data.shape)
 		assert (size >= 30).all(), f'Rotated image must have size (30, 30) but has {rotated_data.shape}.'
 		# noinspection PyTypeChecker
-		position, angle = _find_white_dot(rotated_data)
+		_, angle = _find_white_dot(rotated_data)
 		angels.append(math.degrees(angle))
 	distribution = np.array(angels)
 	# noinspection PyArgumentList
@@ -141,7 +122,7 @@ def test_transform_image():
 		# noinspection PyTypeChecker
 		transformed_data = np.array(transform_image(image, rotate=True, scale=True, tilt=True, scale_mean=2, scale_deviation=0.5, tilt_deviation=0.3, generator=np.random.default_rng()))
 		# noinspection PyTypeChecker
-		position, angle = _find_white_dot(transformed_data)
+		_, angle = _find_white_dot(transformed_data)
 		angels.append(math.degrees(angle))
 		sizes.append((transformed_data > 100).sum())
 	distribution1 = np.array(angels)
@@ -170,7 +151,7 @@ def test_sar():
 		assert sar_data.max() >= 50  # white color from RGB
 		assert (18 <= round(sar_data[:10, :10].mean()) <= 26) and round(sar_data[:10, :10].std()) <= 4  # noise
 		# noinspection PyTypeChecker
-		position, angle = _find_white_dot(sar_data[:, :, 0])
+		_, angle = _find_white_dot(sar_data[:, :, 0])
 		angels.append(math.degrees(angle))
 		sizes.append((sar_data > 100).sum())
 	# noinspection DuplicatedCode
